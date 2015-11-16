f745b964fda5e4fd59c8166a695c12a6ab0cd6a3
mostfamiliar <tlstratton@gmail.com>
2015-11-16 15:36:04 -0800

    JS works!

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
   $("form#pingpong").submit(function(event) {
     var number = parseInt($("input#usernumber").val());
 
-  for (var index = 0; index <= number; index++) {
-    if (number % 3 ===0) {
-      $("ul").append("<li>ping</li>");
-    }
-    else if (number % 5===0) {
-      $("ul").append("<li>pong</li>");
-    }
-    else if (number % 5===0 && number % 3===0) {
+  for (var index = 1; index <= number; index++) {
+    if (index % 5 === 0 && index % 3 === 0) {
       $("ul").append("<li>pingpong</li>");
     }
-    else {
-      //$("ul").append("<li>("number")</li>");
+    else if (index % 3 === 0) {
+        $("ul").append("<li>ping</li>");
+      }
+    else if (index % 5 === 0) {
+      $("ul").append("<li>pong</li>");
     }
-  }
+    else {
+      $("ul").append("<li>"+index+"</li>");
+    }
+  };
   event.preventDefault();
 });
 });
